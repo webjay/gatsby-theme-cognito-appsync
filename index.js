11b10563592a6a250d0eb5fd66f3251faa262481
Jacob Friis Saxberg <jacob@weareheadlight.com>
2020-09-11 11:58:10 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import signout from './src/lib/signout';
 import client from './src/client';
 import './src/config';
-// import AuthWrap from './src/components/auth';
-// import ApolloWrap from './src/components/apollo';
 
 export {
   client,
   signout,
-  // AuthWrap,
-  // ApolloWrap,
 };
