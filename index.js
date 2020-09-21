d784e94899d9d6f46f3061c64b2a1438f4477664
Jacob Friis Saxberg <jacob@weareheadlight.com>
2020-09-21 14:55:57 +0200

    AuthWrap is back

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
 import client from './src/client';
 import userInfo from './src/lib/user';
 import signout from './src/lib/signout';
 import useAuth from './src/hooks/auth';
+import AuthWrap from './src/components/auth';
 
 export {
   client,
   userInfo,
   signout,
   useAuth,
-  AmplifyAuthenticator,
+  AuthWrap,
 };
---
+++ src/components/auth.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { AmplifyAuthenticator } from '@aws-amplify/ui-react';
+import useAuth from '../hooks/auth';
+
+const styleCenter = {
+  minHeight: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+function AuthWrap({ children, center }) {
+  const signedin = useAuth();
+  if (signedin === true) return children;
+  const style = center === true ? styleCenter : {};
+  return <AmplifyAuthenticator style={style} />;
+}
+
+AuthWrap.defaultProps = {
+  center: true,
+};
+
+AuthWrap.propTypes = {
+  children: PropTypes.node.isRequired,
+  center: PropTypes.bool,
+};
+
+export default AuthWrap;
