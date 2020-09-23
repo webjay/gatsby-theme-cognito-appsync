c25304ee4d81f98a29d6940bf0cefebcf51066a0
Jacob Friis Saxberg <jacob@webcom.dk>
2020-09-24 00:41:46 +0200

    get users initial auth state

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -1,14 +1,20 @@
 import { useState, useEffect } from 'react';
 import { onAuthUIStateChange, AuthState } from '@aws-amplify/ui-components';
+import userInfo from '../lib/user';
 
 const { SignedIn } = AuthState;
 
 function useAuth() {
-  const [authState, setAuthState] = useState();
+  const [authState, setAuthState] = useState(null);
   function authDetect() {
+    userInfo().then((username) => {
+      const state = username === null ? false : SignedIn;
+      setAuthState(state);
+    });
     return onAuthUIStateChange(setAuthState);
   }
   useEffect(authDetect, []);
+  if (authState === null) return null;
   return authState === SignedIn;
 }
 
